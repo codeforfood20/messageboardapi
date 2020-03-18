ad0d89c88176668a2a310376435427d7b291a6a6
Anthony Phillips <anthony.phillips2010@live.com>
2020-03-18 14:43:33 -0400

    Deleted old client app.

--- MessageBoard.Api.Old/ClientApp/src/app/messageboard/messageboard.component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MessageBoardComponent = /** @class */ (function () {
-    function MessageBoardComponent() {
-    }
-    return MessageBoardComponent;
-}());
-exports.MessageBoardComponent = MessageBoardComponent;
-//# sourceMappingURL=messageboard.component.js.map
